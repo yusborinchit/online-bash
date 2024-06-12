948d2ee66e57473cb351d18914ee4b450aae5b44
Santiago Garcia <just.b000ringsh1t@gmail.com>
2024-06-12 20:35:24 -0300

    feat: :sparkles: add help log at first

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
-import { bash_form, bash_input } from "./consts/dom-elements";
+import { createCommandLog } from "./components/command-log";
+import { createHelpLog } from "./components/help-log";
+import { bash_form, bash_input, bash_logs } from "./consts/dom-elements";
 import { handleFormSubmit } from "./handlers/form-submit";
 import { handleInputBlur } from "./handlers/input-blur";
 import { handleInputKeyDown } from "./handlers/input-key-down";
 
-bash_form.addEventListener("submit", handleFormSubmit);
+bash_logs.appendChild(createCommandLog("$", "help"));
+bash_logs.append(createHelpLog());
 
+bash_form.addEventListener("submit", handleFormSubmit);
 bash_input.addEventListener("blur", handleInputBlur);
 bash_input.addEventListener("keydown", handleInputKeyDown);
